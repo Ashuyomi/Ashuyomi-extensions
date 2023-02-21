8b9814b893ee813f3ebfe85a779ed0b25d19f5aa
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-02-20 20:24:01 -0400

    Update MadaraGenerator.kt

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -13,6 +13,10 @@ class MadaraGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 28
 
     override val sources = listOf(
+        // Extenções Minhas:
+        SingleLang("Hikari Scan", "https://hikariscan.com.br", "pt-BR", isNsfw = true, overrideVersionCode = 3),
+
+        // Extenções Oficiais
         MultiLang("Leviatan Scans", "https://leviatanscans.com", listOf("en", "es"), className = "LeviatanScansFactory", overrideVersionCode = 13),
         MultiLang("MangaForFree.net", "https://mangaforfree.net", listOf("en", "ko", "all"), isNsfw = true, className = "MangaForFreeFactory", pkgName = "mangaforfree", overrideVersionCode = 1),
         MultiLang("Manhwa18.cc", "https://manhwa18.cc", listOf("en", "ko", "all"), isNsfw = true, className = "Manhwa18CcFactory", pkgName = "manhwa18cc", overrideVersionCode = 4),
